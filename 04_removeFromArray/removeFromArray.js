64dd8b7c91884e7e1626a48f4e2da8fb04bb02c1
Srinath20 <srinathgollapinni@gmail.com>
2022-10-15 19:35:13 +0530

    Completed few of the exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,item) {
+   let arr1 = [...arr];
+   const var1 = arr.length;
+    for(i=0;i<var1;i++){
+        if(arr1[i]==item){
+            arr1[i].splice(i,1);
+        }
+        else
+            return arr1;
+    }
 
 };
 
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,10 @@
-const ftoc = function() {
+const ftoc = function(temp1) {
+  return Math.round((f - 32) * (5/9) * 10) / 10;
+}
 
-};
-
-const ctof = function() {
-
-};
+const ctof = function(temp1) {
+  return Math.round(((c * 9/5) + 32) * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
